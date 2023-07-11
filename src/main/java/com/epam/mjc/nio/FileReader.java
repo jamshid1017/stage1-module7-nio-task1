845f6fa8ac19d79277890472ef593748dae9ea6d
jamshid <jamshid102002@gmail.com>
2023-07-11 10:46:43 +0500

    a

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,36 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+
+            String line = null;
+
+            while ((line = reader.readLine()) != null) {
+
+                if (line.startsWith("Name: ")) {
+                    profile.setName(line.substring(6));
+                } else if (line.startsWith("Age: ")) {
+                    profile.setAge(Integer.parseInt(line.substring(5)));
+                } else if (line.startsWith("Email: ")) {
+                    profile.setEmail(line.substring(7));
+                } else if (line.startsWith("Phone: ")) {
+                    profile.setPhone(Long.parseLong(line.substring(7)));
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return profile;
     }
 }
